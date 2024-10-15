561a00242462b86b5994223beb321e35927ede2b
Roland Grinis <roland.grinis@grinisrit.com>
2024-10-15 15:34:01 +0100

    fix autograd call

--- example-app.cpp
+++ example-app.cpp
@@ -1,7 +1,6 @@
 #include <torch/torch.h>
-#include <torch/csrc/autograd/variable.h>
-#include <torch/csrc/autograd/function.h>
 
+#define TORCH_USE_CXX11_ABI 0
 #include <iostream>
 
 int main() {
@@ -12,11 +11,13 @@ int main() {
     std::cout << "Autograd prep\n";
     torch::Tensor a = 10*torch::ones({2, 3}, torch::requires_grad());
     //torch::Tensor b = torch::randn({2, 3});
-    torch::Tensor c = a*a*tensor;
+    torch::Tensor c = (a*a*tensor).sum();
     std::cout << "a: " << a << "\n";
     std::cout << "c: " << c << "\n";
     std::cout << "Autograd call\n";
-    torch::autograd::grad({c}, {a});
+    //torch::autograd::grad({c}, {a});
+    const auto a_grad = torch::autograd::grad({c}, {a});
+    std::cout << "a_grad: " << a_grad.at(0) << "\n";
     //c.backward(); // a.grad() will now hold the gradient of c w.r.t. a.
     //std::cout << a << "\n";
     std::cout << "Done\n";
