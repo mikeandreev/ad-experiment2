aa2069f9987ae4341ff7d16bb239a8cd2a7e0c92
mike_va@mail.ru <Mihail Andreev>
2024-10-18 17:37:50 +0300

    fix and update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ad-experiment2
+
+## LibTorch
+
+### Install
+Take the archive as described here
+https://pytorch.org/cppdocs/installing.html
+
+Unpack it to be at `ext/libtorch`.
+
+
--- example-app.cpp
+++ example-app.cpp
@@ -12,12 +12,12 @@ int main() {
     std::cout << "Autograd prep\n";
     torch::Tensor a = 10*torch::ones({2, 3}, torch::requires_grad());
     //torch::Tensor b = torch::randn({2, 3});
-    torch::Tensor c = a*a*tensor;
+    torch::Tensor c = (a*a + tensor).sum();
     std::cout << "a: " << a << "\n";
     std::cout << "c: " << c << "\n";
     std::cout << "Autograd call\n";
-    torch::autograd::grad({c}, {a});
+    const auto da = torch::autograd::grad({c}, {a});
     //c.backward(); // a.grad() will now hold the gradient of c w.r.t. a.
-    //std::cout << a << "\n";
+    std::cout << da << "\n";
     std::cout << "Done\n";
 }
